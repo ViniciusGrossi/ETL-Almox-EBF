f8a05372c9326bdce6db7e31a179d591c4a46412
Vinicius Grossi <viniciussggrossi@gmail.com>
2024-08-08 11:16:09 -0300

    Melhora no mecanismo de atualização de base de dados. tab3 e tab4

--- pages/Transformacao.py
+++ pages/Transformacao.py
@@ -16,17 +16,11 @@ def process_file(uploaded_file):
     
     # Remover linhas em branco na coluna 'CODIGO'
     df = df.dropna(subset=['CODIGO'])
-    
-    # Remover linhas onde 'CODIGO' começa com '0'
-    df = df[~df['CODIGO'].astype(str).str.startswith('0')]
 
     # Criar coluna 'CODIGO' com valores que começam com '5' e têm exatamente 12 dígitos na coluna 'A'
     df['CODIGO'] = df['CODIGO'].astype(str).str.replace(r'\s0$', '', regex=True)
     df['CÓDIGO'] = df['CODIGO'].astype(str).str.extract(r'(\b5\d{11}\b)')
     df_codigo = df.dropna(subset=['CÓDIGO'])
-    
-    # Remover duplicatas na coluna 'CODIGO'
-    df_codigo = df_codigo.drop_duplicates(subset=['CÓDIGO'])
 
     # Criar coluna 'DESCRICAO' com valores da coluna 'B'
     df_codigo['DESCRICAO'] = df_codigo['DESCRICAO'].str.strip()
@@ -76,6 +70,17 @@ def adjust_quantities(base_file, update_file):
     
     return df_base
 
+# Função para atualizar uma coluna específica baseada no mapeamento de CODIGO
+def atualizar_coluna_selecionada(base_dados, tabela_referencia, coluna):
+    # Criar um dicionário para mapear CODIGO para a coluna selecionada
+    mapa_referencia = dict(zip(tabela_referencia['CODIGO'], tabela_referencia[coluna]))
+    
+    # Atualizar a coluna selecionada na base de dados original
+    base_dados[coluna] = base_dados['CODIGO'].map(mapa_referencia)
+    
+    return base_dados
+
+# Função para exportar DataFrame para Excel
 def to_excel(df):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
@@ -87,11 +92,11 @@ def main():
     st.title("Sistema de Limpeza e Transformação de Dados")
 
     # Criação de abas
-    tab1, tab2 = st.tabs(["Transformação", "Ajuste de Base de Dados"])
+    tab1, tab2, tab3, tab4 = st.tabs(["Transformação", "Ajuste de quantidade", "Atualização de Referência", "Atualização de Colunas"])
 
     with tab1:
         st.header("Transformação")
-        uploaded_file = st.file_uploader("Escolha um arquivo Excel do ERP", type="xlsx")
+        uploaded_file = st.file_uploader("Escolha um arquivo Excel do ERP", type="xlsx", key="erp_file_tab1")
         if uploaded_file is not None:
             df_resultado = process_file(uploaded_file)
             
@@ -100,7 +105,6 @@ def main():
             
             processed_data = to_excel(df_resultado)
             
-            # Baixar o arquivo processado
             st.download_button(
                 label="Baixar Arquivo Processado",
                 data=processed_data,
@@ -110,20 +114,87 @@ def main():
 
     with tab2:
         st.header("Ajuste de Base de Dados")
-        base_file = st.file_uploader("Escolha um arquivo Excel da base de dados original", type="xlsx")
-        update_file = st.file_uploader("Escolha o arquivo Excel de atualização (transformado)", type="xlsx")
+        base_file = st.file_uploader("Escolha um arquivo Excel da base de dados original", type="xlsx", key="base_file_tab2")
+        update_file = st.file_uploader("Escolha o arquivo Excel de atualização (transformado)", type="xlsx", key="update_file_tab2")
         if base_file is not None and update_file is not None:
             df_resultado = adjust_quantities(base_file, update_file)
             
             adjusted_data = to_excel(df_resultado)
             
-            # Baixar o arquivo ajustado
             st.download_button(
                 label="Baixar Arquivo Ajustado",
                 data=adjusted_data,
                 file_name="dados_ajustados.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
+
+    with tab3:
+        st.header("Atualização de Referência")
+        base_dados_file = st.file_uploader("Upload da base de dados original", type=["xlsx"], key="base_dados_tab3")
+        if base_dados_file is not None:
+            base_dados = pd.read_excel(base_dados_file)
+            st.write("Base de Dados Original:")
+            st.dataframe(base_dados)
+
+        tabela_referencia_file = st.file_uploader("Upload da tabela com a coluna referência atualizada", type=["xlsx"], key="tabela_referencia_tab3")
+        if tabela_referencia_file is not None:
+            tabela_referencia = pd.read_excel(tabela_referencia_file)
+            st.write("Tabela de Referência Atualizada:")
+            st.dataframe(tabela_referencia)
+
+        if base_dados_file is not None and tabela_referencia_file is not None:
+            if st.button("Atualizar Referência", key="update_button_tab3"):
+                base_dados = atualizar_referencia(base_dados, tabela_referencia)
+                st.success("A coluna REFERENCIA foi atualizada com sucesso!")
+                st.write("Base de Dados Atualizada:")
+                st.dataframe(base_dados)
+
+                updated_data = to_excel(base_dados)
+                
+                st.download_button(
+                    label="Baixar Base de Dados Atualizada",
+                    data=updated_data,
+                    file_name="base_dados_atualizada.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+
+    with tab4:
+        st.header("Atualização de Colunas")
+
+        base_dados_file = st.file_uploader("Upload da base de dados original", type=["xlsx"], key="base_dados_tab4")
+        if base_dados_file is not None:
+            base_dados = pd.read_excel(base_dados_file)
+            st.write("Base de Dados Original:")
+            st.dataframe(base_dados)
+
+        tabela_referencia_file = st.file_uploader("Upload da tabela com as colunas atualizadas", type=["xlsx"], key="tabela_referencia_tab4")
+        if tabela_referencia_file is not None:
+            tabela_referencia = pd.read_excel(tabela_referencia_file)
+            st.write("Tabela de Referência Atualizada:")
+            st.dataframe(tabela_referencia)
+
+        colunas_selecionadas = st.multiselect(
+            "Selecione as colunas que deseja atualizar:",
+            ['ENDERECO', 'QUANTIDADE', 'MARCA', 'REFERENCIA', 'UNIDADE MEDIDA', 'U.N', 'COD MARCA'],
+            key="colunas_selecionadas_tab4"
+        )
+
+        if base_dados_file is not None and tabela_referencia_file is not None and colunas_selecionadas:
+            if st.button("Atualizar Colunas", key="update_button_tab4"):
+                for coluna in colunas_selecionadas:
+                    base_dados = atualizar_coluna_selecionada(base_dados, tabela_referencia, coluna)
+                st.success(f"As colunas {', '.join(colunas_selecionadas)} foram atualizadas com sucesso!")
+                st.write("Base de Dados Atualizada:")
+                st.dataframe(base_dados)
+
+                updated_data = to_excel(base_dados)
+                
+                st.download_button(
+                    label="Baixar Base de Dados Atualizada",
+                    data=updated_data,
+                    file_name="base_dados_atualizada_colunas.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
 
 if __name__ == "__main__":
     main()
